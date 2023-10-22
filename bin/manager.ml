39a0094a1e12e3560171856efc1a97489bd73a5f
Derek <det75@cornell.edu>
2023-10-22 15:12:28 -0400

    Add Makefile
    - Added Makefile similar to A2
    - Modified makefile so that "make manager" runs manager.ml as executable
    - renamed "src" folder to "lib"

--- bin/manager.ml
+++ bin/manager.ml
@@ -1,12 +1,20 @@
 open Cards
 
+let () = print_endline "hello world"
+
 module Game = struct
   type t = {
-    player_hand : Cards.Card.t list;
-    enemy_hand : Cards.Card.t list;
+    player_hand : Card.t list;
+    enemy_hand : Card.t list;
   }
 
-  let print_hands (game : t) = failwith "unimplemented"
+  (* Prints [card] *)
+  let print_card (card : Card.t) : unit = print_string "[placeholder]"
+
+  (* Prints the hands of the player_hand and the enemy hand. *)
+  let print_hands (game : t) : unit =
+    List.iter print_card game.player_hand;
+    List.iter print_card game.enemy_hand
 
   let rec draw (hand : Cards.Card.t list) (n : int) : Cards.Card.t list =
     if n = 0 then hand
