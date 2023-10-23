0765d6c4505433054df9440ca111544d2a85bcd1
Derek <det75@cornell.edu>
2023-10-22 23:16:28 -0400

    Restructure Game module
    - Moved Game module to its own library
    - Modified 3 dune files to reflect this change

--- bin/manager.ml
+++ bin/manager.ml
@@ -1,24 +1,3 @@
-open Cards
+open Game
 
-let () = print_endline "hello world"
-
-module Game = struct
-  type t = {
-    player_hand : Card.t list;
-    enemy_hand : Card.t list;
-  }
-
-  (* Prints [card] *)
-  let print_card (card : Card.t) : unit = print_string "[placeholder]"
-
-  (* Prints the hands of the player_hand and the enemy hand. *)
-  let print_hands (game : t) : unit =
-    List.iter print_card game.player_hand;
-    List.iter print_card game.enemy_hand
-
-  let rec draw (hand : Cards.Card.t list) (n : int) : Cards.Card.t list =
-    if n = 0 then hand
-    else
-      let random_card = Card.get_rand_card in
-      draw (hand @ [ random_card ]) (n - 1)
-end
+let () = print_endline "Welcome to Crazy Uno!"
