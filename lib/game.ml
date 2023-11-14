0efbb9b4382ae1d37dc1638ceda03306e57a0485
Jacob Seto <jjs472@cornell.edu>
2023-11-14 16:47:51 -0500

    Opponent ml file
    - opp.ml will contain all game logic pertaining to the opponent

--- lib/game.ml
+++ lib/game.ml
@@ -1,9 +1,9 @@
 open Cards
-
+open Opp
 module Game = struct
   type t = {
-    player_hand : Card.t list;
-    enemy_hand : Card.t list;
+    player_hand : Card.t array;
+    enemy_hand : Card.t array;
   }
 
   (* Prints the color and number of [card]. Example output: "[Yellow 5]"*)
@@ -16,9 +16,9 @@ module Game = struct
   (* Prints the hands of the player_hand and the enemy hand. *)
   let print_hands (game : t) : unit =
     print_string "Player's hand:";
-    List.iter print_card game.player_hand;
+    List.iter print_card (Array.to_list game.player_hand);
     print_string "\nEnemy's hand:";
-    List.iter print_card game.enemy_hand
+    List.iter print_card (Array.to_list game.enemy_hand)
 
   let rec draw (hand : Cards.Card.t list) (n : int) : Cards.Card.t list =
     let local_rng = Random.State.make_self_init () in
---
+++ lib/opp.ml
@@ -0,0 +1,10 @@
+open Cards
+
+module AI = struct
+  (**Given a hand, a random card will be selected to be placed on the pile. If
+     the play is illegal, another card will be randomly selected*)
+  let play_card (hand : Card.t array) : unit = 
+    
+    
+    failwith "unimplemented"
+end
