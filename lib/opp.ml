911e0c0c36f08e0d87eea21a131e570ff4a69727
Jacob Seto <jjs472@cornell.edu>
2023-11-16 13:49:33 -0500

    Update opp.ml

--- lib/opp.ml
+++ lib/opp.ml
@@ -3,7 +3,8 @@ open Cards
 module AI = struct
   (**Given a hand, a random card will be selected to be placed on the pile. If
      the play is illegal, another card will be randomly selected*)
-  let play_card (hand : Card.t array) : unit = 
+  let rec play_card (hand : Card.t array) : unit = 
+    let ran_card = hand.(Random.int (Array.length hand)) in 
     
     
     failwith "unimplemented"
