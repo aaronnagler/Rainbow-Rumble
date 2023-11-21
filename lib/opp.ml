aa98918eeb2b02435480900573a514909132dad0
Jacob Seto <jjs472@cornell.edu>
2023-11-20 20:30:36 -0500

    Revert back to using list

--- lib/opp.ml
+++ lib/opp.ml
@@ -1,11 +1,7 @@
 open Cards
-
 module AI = struct
   (**Given a hand, a random card will be selected to be placed on the pile. If
      the play is illegal, another card will be randomly selected*)
-  let rec play_card (hand : Card.t array) : unit = 
-    let ran_card = hand.(Random.int (Array.length hand)) in 
-    
-    
-    failwith "unimplemented"
+  (* let rec play_card (game) = let ran_card = List.nth (game.player_hand)
+     (Random.int (List.Alength hand)) in game. *)
 end
