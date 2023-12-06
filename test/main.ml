ea3d81953d87e83a4b9c3a0d4eaa5a8bd98f27e2
Derek <det75@cornell.edu>
2023-12-06 16:51:45 -0500

    Start test suite backbone

--- test/main.ml
+++ test/main.ml
@@ -1,4 +1,10 @@
 open OUnit2
+open Game
+open Card
 
-let tests = "cards test suite" >::: []
-let _ = run_test_tt_main tests
+let t1 =
+  [ (* "get_color" >:: assert_equal "Red" (get_color (make_card "Red" "2"
+       "None")); *) ]
+
+let tests = "cards test suite" >::: t1
+let () = run_test_tt_main tests
