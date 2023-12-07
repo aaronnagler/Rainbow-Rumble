641773c2aa93e08b915dac85493ae0925150cc1f
Derek <det75@cornell.edu>
2023-12-07 15:26:22 -0500

    Add test cases for card functions
    - get_color, get_number get_property_name, get_property_description

--- test/main.ml
+++ test/main.ml
@@ -4,14 +4,38 @@ open Card
 
 let card_tests =
   [
-    ( "get_color trivial case" >:: fun _ ->
+    ( "get_color regular card" >:: fun _ ->
       assert_equal "Red" (get_color (make_card "Red" "2" "None")) );
-    ( "get_color card with property" >:: fun _ ->
+    ( "get_color draw 2" >:: fun _ ->
       assert_equal "Blue" (get_color (make_card "Blue" "2" "Draw 2")) );
-    ( "get_color" >:: fun _ ->
-      assert_equal "Yellow" (get_color (make_card "Yellow" "2" "Draw 4")) );
-    ( "get_number" >:: fun _ ->
-      assert_equal "5" (get_number (make_card "Red" "5" "None")) );
+    ( "get_color draw 4" >:: fun _ ->
+      assert_equal "Wild" (get_color (make_card "Wild" "2" "Draw 4")) );
+    ( "get_number regular card" >:: fun _ ->
+      assert_equal "5" (get_number (make_card "Yellow" "5" "None")) );
+    ( "get_number draw 2" >:: fun _ ->
+      assert_equal "NaN" (get_number (make_card "Red" "NaN" "Draw 2")) );
+    ( "get_number draw 4" >:: fun _ ->
+      assert_equal "NaN" (get_number (make_card "Wild" "NaN" "Draw 4")) );
+    ( "get_number wild" >:: fun _ ->
+      assert_equal "NaN" (get_number (make_card "Wild" "NaN" "None")) );
+    ( "get_property_name regular card" >:: fun _ ->
+      assert_equal "None" (get_property_name (make_card "Green" "2" "None")) );
+    ( "get_property_name draw 2" >:: fun _ ->
+      assert_equal "Draw 2"
+        (get_property_name (make_card "Green" "NaN" "Draw 2")) );
+    ( "get_property_name draw 4" >:: fun _ ->
+      assert_equal "Draw 4"
+        (get_property_name (make_card "Wild" "NaN" "Draw 4")) );
+    ( "get_property_name wild" >:: fun _ ->
+      assert_equal "None" (get_property_name (make_card "Wild" "NaN" "None")) );
+    ( "get_property_description regular card" >:: fun _ ->
+      assert_equal "None" (get_property_name (make_card "Red" "4" "None")) );
+    ( "get_property_description draw 2" >:: fun _ ->
+      assert_equal "Opponent must draw 2 cards from the top of the deck"
+        (get_property_description (make_card "Blue" "NaN" "Draw 2")) );
+    ( "get_property_description draw 4" >:: fun _ ->
+      assert_equal "Opponent must draw 4 cards from the top of the deck"
+        (get_property_description (make_card "Wild" "NaN" "Draw 4")) );
   ]
 
 let suite = "test suite for cards" >::: List.flatten [ card_tests ]
