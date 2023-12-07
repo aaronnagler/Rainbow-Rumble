fcf42e61686e51a09c483565c260dbe847233d49
Ben Perl <bdp58@cornell.edu>
2023-12-07 15:13:25 -0500

    is_legal_play now takes correct wild card plays into account

--- lib/game.ml
+++ lib/game.ml
@@ -64,9 +64,18 @@ module Game = struct
 
   (* Returns true if a card can legally be played on the discard_pile. *)
   let is_legal_play (card : Card.t) (discard_pile : Card.t) : bool =
-    if Card.get_color card = Card.get_color discard_pile then true
-    else if Card.get_number card = Card.get_number discard_pile then true
-    else Card.get_color card = "Wild"
+    let same_color, same_number =
+      ( Card.get_color card = Card.get_color discard_pile,
+        Card.get_number card = Card.get_number discard_pile )
+    in
+    match (same_color, same_number) with
+    | true, _ | _, true -> true
+    | false, false -> Card.get_property_name discard_pile = "None"
+
+  (* Old Stuff *)
+  (* if Card.get_color card = Card.get_color discard_pile then true else if
+     Card.get_number card = Card.get_number discard_pile then true else
+     Card.get_color card = "Wild" *)
 
   (* Removes [card] from [hand] Returns: [hand] without [card] *)
   let rec remove_card (card : Card.t) = function
